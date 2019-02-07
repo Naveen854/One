ad4355383c5ce4e533ecc32fb2b8790cd590643e
Naveen854 <46414913+Naveen854@users.noreply.github.com>
2019-02-07 21:27:45 +0530

    Update lab1_marixinverse_4th_jan19.py

--- lab1_marixinverse_4th_jan19.py
+++ lab1_marixinverse_4th_jan19.py
@@ -1,14 +1,14 @@
 # It is a program to find inverse of a matrix using numpy
-import numpy as np
-print('Enter the square matrix of any order ;') 
+import numpy as np 
 print('Enter the matrix in the form [[a11,a12,a.....a1n],........,[ai1,....,ajn]]')
 a=input()
 d=np.linalg.det(a)
+print('Determinant = %d',d)
 if(d==0):
 	print('Determinant cannot be found since Determinant=0')
 else: 
 	i=np.linalg.inv(a)
-	print('Determinant of given matrix is ;')
+	print('Inverse of given matrix is ;')
 	print i
 p=np.linalg.pinv(a)
 print('Psuedo Inverse of given marix is ;')
